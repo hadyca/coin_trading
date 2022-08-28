2bc2ba9bd55ed700ed00e4e95cd9f875590cd49a
Jae Hyung Kim <SC425@ypsa.com>
2022-08-28 14:35:24 +0600

    빗썸헤더완료

--- api/accounts/accountsBS.js
+++ api/accounts/accountsBS.js
@@ -1,52 +1,25 @@
-const axios = require("axios").default;
+import axios from "axios";
 require("dotenv").config();
-import request from "request";
-import cryptojsHmacSHA512 from "crypto-js/hmac-sha512";
-
-const api_base = "https://api.bithumb.com";
-const api_private_info = {
-  apiKey: process.env.BITHUMB_OPEN_API_ACCESS_KEY,
-  secretKey: process.env.BITHUMB_OPEN_API_SECRET_KEY,
-};
-
-const req_query = {};
-
-// const api_key = process.env.BITHUMB_OPEN_API_ACCESS_KEY;
-// const api_secret = process.env.BITHUMB_OPEN_API_SECRET_KEY;
-
-// const xcoinAPI = new XCoinAPI(api_key, api_secret);
-// const rgParams = {
-//   order_currency: "BTC",
-//   payment_currency: "KRW",
-// };
+import bithumbHeader from "../../component/bithumbHeader";
 
 export default async function accountsBS() {
-  // const res = await xcoinAPI.xcoinApiCall("/info/account", rgParams);
-  // console.log(res.body);
+  try {
+    const req_query = {
+      endpoint: "/info/account",
+      order_currency: "BTC",
+      payment_currency: "KRW",
+    };
 
-  const api_base = "https://api.bithumb.com";
-  const api_private_info = {
-    apiKey: process.env.BITHUMB_OPEN_API_ACCESS_KEY,
-    secretKey: process.env.BITHUMB_OPEN_API_SECRET_KEY,
-  };
-
-  const req_query = {
-    endPoint: "/info/account",
-    order_currency: "BTC",
-    payment_currency: "KRW",
-  };
-
-  const make_header = (obj) => {
-    let output_string = [];
-    Object.keys(obj).forEach((val) => {
-      let key = val;
-      key = encodeURIComponent(key.replace(/[!'()*]/g, escape));
-      let value = encodeURIComponent(obj[val].replace(/[!'()*]/g, escape));
-      output_string.push(key + "=" + value);
-    });
-
-    return output_string.join("&");
-  };
-
-  console.log(make_header(req_query));
+    const result = await axios.post(
+      "https://api.bithumb.com/info/account",
+      req_query,
+      {
+        headers: bithumbHeader(req_query),
+      }
+    );
+    console.log(result.data);
+    return result.data;
+  } catch (error) {
+    console.log("accountError!", error);
+  }
 }
