6e94ea5a56476774d9cb733032b821673f861244
Jae Hyung Kim <SC425@ypsa.com>
2022-09-22 16:27:29 +0600

    코인

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const test = async () => {
 
 // test();
 
-// const requestApi = () => setInterval(test, 1000);
+const requestApi = () => setInterval(test, 1000);
 
-// requestApi();
+requestApi();
 
 //bid 매수, ask 매도
